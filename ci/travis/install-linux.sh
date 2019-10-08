8897879ebb10880c082ef59465b3af977837f7a6
AntonElagin <anton120299@gmail.com>
2019-10-08 16:14:50 +0300

    Changed CMakeFile

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -27,3 +27,11 @@ if [ "$TARGET_CPU" == "x86" ]; then
     # g++-multilib ставим в самом конце, после i386-пакетов!
     sudo apt-get install -y g++-5-multilib
 fi
+sudo apt-get install libgtest-dev
+sudo apt-get install cmake # install cmake
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+ 
+# copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
+sudo cp *.a /usr/lib
