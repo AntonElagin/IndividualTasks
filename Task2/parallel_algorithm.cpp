cd704759ba7023136296acc115feff35ebb8520b
AntonElagin <anton120299@gmail.com>
2019-10-26 10:58:41 +0300

    Changed parallel algorithm

--- Task2/parallel_algorithm.cpp
+++ Task2/parallel_algorithm.cpp
@@ -2,19 +2,20 @@
 // Created by anton on 20.10.2019.
 //
 
+#include <wait.h>
 #include "parallel_algorithm.h"
 
 int find_zero_count_parallel(const Comment* array, int arr_size) {
-  pid_t child;
   size_t process_count = 4;
+  pid_t* child = (pid_t*) malloc(4 * sizeof(pid_t));
   int return_counter = 0;
   for (int i = 0; i < process_count; ++i) {
     int fd[2];
     int delta = arr_size / process_count;
     pipe(fd);
-    if ((child = fork()) < 0)
+    if ((child[i] = fork()) < 0)
       return -1;
-    else if (child == 0) {
+    else if (child[i] == 0) {
       int counter = find_zero_count(array, i * delta, (i + 1) * delta);
       close(fd[0]);
       write(fd[1], &counter, sizeof(int));
@@ -26,6 +27,11 @@ int find_zero_count_parallel(const Comment* array, int arr_size) {
       read(fd[0], &count, sizeof(int));
       return_counter += count;
     }
+
+    for (int j = 0; j < process_count; ++j) {
+      waitpid(child[j],EXIT_SUCCESS,WUNTRACED);
+    }
+
   }
 
   return return_counter;
